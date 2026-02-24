50fbc11501b9954b29bbaccf3ba30ba6216fd3b3
Rrok Gjinaj <did-48@systemceramics.com>
2026-02-24 12:07:14 +0100

    feat: add 404.html for Angular routing and enable hash location in router configuration

--- k2k-app/src/app/app.config.ts
+++ k2k-app/src/app/app.config.ts
@@ -1,5 +1,5 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter, withInMemoryScrolling } from '@angular/router';
+import { provideRouter, withHashLocation, withInMemoryScrolling } from '@angular/router';
 
 import { routes } from './app.routes';
 
@@ -8,7 +8,8 @@ export const appConfig: ApplicationConfig = {
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(
       routes,
-      withInMemoryScrolling({ scrollPositionRestoration: 'top', anchorScrolling: 'enabled' })
+      withInMemoryScrolling({ scrollPositionRestoration: 'top', anchorScrolling: 'enabled' }),
+      //withHashLocation()
     )
   ]
 };
